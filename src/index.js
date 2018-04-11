486bf8a9264358a0365e45e57bb0d4e7d7ed4376
yiphei <yanyiphei@gmail.com>
2018-04-11 15:26:51 -0400

    message

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+
+// setInterval(function() => {
+//   num = num + 1;
+//   console.log('hello');
+//
+//   $('#main').html('You have been here for ${num} seconds..');
+//
+//    }, 1000);
+
+
+setInterval(() => {
+  num += 1;
+  console.log(num);
+  console.log('hello');
+  const a = `You have been here for ${num} seconds..`;
+
+  $('#main').html(a);
+}, 1000);
